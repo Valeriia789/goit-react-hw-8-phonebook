7ba7522641eac80f8b3c36d7253081af927b5c10
Valeriia <valeriia.air@gmail.com>
2022-10-15 21:10:19 +0300

    routes/ layout

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,29 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import { NavLink } from 'react-router-dom';
+import { AppBar } from '../AppBar/AppBar';
 
 const SharedLayout = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/register">Registration</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contacts">Contacts</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Suspense fallback={<div>Loading page...</div>}>
+    <>
+      <AppBar />
+      <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </div>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme';
@@ -11,11 +12,13 @@ import { theme } from './theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/goit-react-hw-8-phonebook">
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter basename="/goit-react-hw-8-phonebook">
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
