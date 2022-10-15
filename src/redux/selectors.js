044c1c16575c72b30bcd1f01f5fb421940045a7b
Valeriia <valeriia.air@gmail.com>
2022-10-15 21:08:45 +0300

    auth redux

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,4 +3,10 @@ export const selectFilter = state => state.filter;
 // export const selectIsLoading = state => state.contacts.isLoading;
 // export const selectError = state => state.contacts.error;
 
-
+export const getContacts = state => state.contacts.items;
+export const getIsLoading = state => state.contacts.isLoading;
+export const getError = state => state.contacts.error;
+export const getFilter = state => state.filter;
+export const selectLoading = state => state.contacts.loading;
+// export const selectFilter = state => state.contacts.filter;
+export const selectAllContacts = state => state.contacts.items;
