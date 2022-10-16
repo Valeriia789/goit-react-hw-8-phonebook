0c596948a1eb0c09baba6b2cfa32cfd0c4f294d0
Valeriia <valeriia.air@gmail.com>
2022-10-16 23:05:25 +0300

    видалено зайвий комент

--- src/components/ContactsEditorForm/ContactsEditorForm.js
+++ src/components/ContactsEditorForm/ContactsEditorForm.js
@@ -15,8 +15,6 @@ export const ContactsEditorForm = () => {
     const name = form.elements.name.value;
     const number = form.elements.number.value;
 
-    console.log(name, number);
-
     if (namesArray.find(myContact => myContact === name)) {
       alert(`${name} is already in contacts`);
       return;
